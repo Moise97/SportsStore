a9207d5d460adebb0bdd5253f8a487601a53c38f
Solcan Moise <solcan_moise2014@yahoo.com>
2019-05-11 20:07:22 +0300

    Handle the Order Process

--- angularjs/controllers/sportsStore.js
+++ angularjs/controllers/sportsStore.js
@@ -1,17 +1,40 @@
 angular.module("sportsStore")
-        .constant("dataUrl", "http://localhost:5500/products")
-        .controller('sportsStoreCtrl', function($scope, $http, dataUrl){
-            $scope.data = {};
-
-            $http({
-                method: 'GET',
-                url: dataUrl
-            })
-            .then(response => {
-                $scope.data.products = response.data;
-            })
-            .catch(error => {
-                $scope.data.error = error;
-            });
-
-        });
+.constant("dataUrl", "http://localhost:5500/products")
+.constant("orderUrl", "http://localhost:5500/orders")
+.controller('sportsStoreCtrl', function($scope, $http, $location, dataUrl, orderUrl, cart){
+    $scope.data = {};
+    
+    $http({
+        method: 'GET',
+        url: dataUrl
+    })
+    .then(response => {     // success
+        $scope.data.products = response.data;
+    })
+    .catch(error => {       // failed
+        $scope.data.error = error;
+    });
+    
+    $scope.sendOrder = function(shippingDetails){
+        
+        let order = angular.copy(shippingDetails);
+        order.products = cart.getProducts();
+        
+        $http({
+            url: orderUrl,
+            method: "POST",
+            data: order
+        })
+        .then(function(response) {  // success
+            $scope.data.orderId = response.data.id;
+            cart.getProducts().length = 0;
+        })
+        .catch(function(error) {  // failed
+            $scope.data.orderError = error;
+        })
+        .finally(function(){
+            $location.path("/complete");
+        })
+    }
+    
+});
