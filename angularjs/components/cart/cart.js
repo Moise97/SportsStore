4c37f73c59973a238e64b8e1808565178567dd8b
moise <solcan_moise2014@yahoo.com>
2020-06-15 22:43:18 +0300

    Indent files

--- angularjs/components/cart/cart.js
+++ angularjs/components/cart/cart.js
@@ -1,74 +1,74 @@
 angular.module("cart", [])
-.factory("cart", function(){
-    let cartData = [];
-    
-    return {
-        
-        /**
-        * Adds the specified product to the cart or increment the number
-        * required if the cart is already contains the product
-        * @param {string} id - The id of the product
-        * @param {string} name - The name of the product
-        * @param {string} price - The price of the product
-        */
-        addProduct: function(id, name, price){
-            let addedToExistingItem = false;
-            for(let i = 0; i < cartData.length; i++){
-                if(cartData[i].id == id){
-                    cartData[i].count++;
-                    addedToExistingItem = true;
-                    break;
+    .factory("cart", function () {
+        let cartData = [];
+
+        return {
+
+            /**
+             * Adds the specified product to the cart or increment the number
+             * required if the cart is already contains the product
+             * @param {string} id - The id of the product
+             * @param {string} name - The name of the product
+             * @param {string} price - The price of the product
+             */
+            addProduct: function (id, name, price) {
+                let addedToExistingItem = false;
+                for (let i = 0; i < cartData.length; i++) {
+                    if (cartData[i].id === id) {
+                        cartData[i].count++;
+                        addedToExistingItem = true;
+                        break;
+                    }
                 }
-            }
-            if(!addedToExistingItem){
-                cartData.push({count: 1, id : id, price: price, name: name});
-            }
-        },
-        
-        /**
-        * Removes the product with the specified id
-        * @param {string} id - The id of the product
-        */
-        removeProduct: function(id){
-            for(let i = 0; i < cartData.length; i++){
-                if(cartData[i].id == id){
-                    cartData.splice(i, 1);
-                    break;
+                if (!addedToExistingItem) {
+                    cartData.push({count: 1, id: id, price: price, name: name});
                 }
-            }
-        },
-        
-        /**
-        * Returns the array of objects in the cart
-        */
-        getProducts: function(){
-            return cartData;
-        }
-    }
-})
-.directive("cartSummary", function(cart){
-    return {
-        restrict: "E",
-        templateUrl: "angularjs/components/cart/cartSummary.html",
-        controller: function($scope) {
-            
-            let cartData = cart.getProducts();
-            
-            $scope.total = function(){
-                let total = 0;
-                for(let i = 0; i < cartData.length; i++){
-                    total += (cartData[i].price * cartData[i].count);
+            },
+
+            /**
+             * Removes the product with the specified id
+             * @param {string} id - The id of the product
+             */
+            removeProduct: function (id) {
+                for (let i = 0; i < cartData.length; i++) {
+                    if (cartData[i].id === id) {
+                        cartData.splice(i, 1);
+                        break;
+                    }
                 }
-                return total;
-            }
-            
-            $scope.itemCount = function(){
-                let total = 0;
-                for(let i = 0; i < cartData.length; i++){
-                    total += cartData[i].count;
-                }
-                return total;
+            },
+
+            /**
+             * Returns the array of objects in the cart
+             */
+            getProducts: function () {
+                return cartData;
             }
         }
-    };
-});
+    })
+    .directive("cartSummary", function (cart) {
+        return {
+            restrict: "E",
+            templateUrl: "angularjs/components/cart/cartSummary.html",
+            controller: function ($scope) {
+
+                let cartData = cart.getProducts();
+
+                $scope.total = function () {
+                    let total = 0;
+                    for (let i = 0; i < cartData.length; i++) {
+                        total += (cartData[i].price * cartData[i].count);
+                    }
+                    return total;
+                };
+
+                $scope.itemCount = function () {
+                    let total = 0;
+                    for (let i = 0; i < cartData.length; i++) {
+                        total += cartData[i].count;
+                    }
+                    return total;
+                }
+            }
+        };
+    });
--- angularjs/controllers/sportsStore.js
+++ angularjs/controllers/sportsStore.js
@@ -1,40 +1,40 @@
 angular.module("sportsStore")
-.constant("dataUrl", "http://localhost:5500/products")
-.constant("orderUrl", "http://localhost:5500/orders")
-.controller('sportsStoreCtrl', function($scope, $http, $location, dataUrl, orderUrl, cart){
-    $scope.data = {};
-    
-    $http({
-        method: 'GET',
-        url: dataUrl
-    })
-    .then(response => {     // success
-        $scope.data.products = response.data;
-    })
-    .catch(error => {       // failed
-        $scope.data.error = error;
-    });
-    
-    $scope.sendOrder = function(shippingDetails){
-        
-        let order = angular.copy(shippingDetails);
-        order.products = cart.getProducts();
-        
+    .constant("dataUrl", "http://localhost:5500/products")
+    .constant("orderUrl", "http://localhost:5500/orders")
+    .controller('sportsStoreCtrl', function ($scope, $http, $location, dataUrl, orderUrl, cart) {
+        $scope.data = {};
+
         $http({
-            url: orderUrl,
-            method: "POST",
-            data: order
+            method: 'GET',
+            url: dataUrl
         })
-        .then(function(response) {  // success
-            $scope.data.orderId = response.data.id;
-            cart.getProducts().length = 0;
-        })
-        .catch(function(error) {  // failed
-            $scope.data.orderError = error;
-        })
-        .finally(function(){
-            $location.path("/complete");
-        })
-    }
-    
-});
+            .then(response => {     // success
+                $scope.data.products = response.data;
+            })
+            .catch(error => {       // failed
+                $scope.data.error = error;
+            });
+
+        $scope.sendOrder = function (shippingDetails) {
+
+            let order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+
+            $http({
+                url: orderUrl,
+                method: "POST",
+                data: order
+            })
+                .then(function (response) {  // success
+                    $scope.data.orderId = response.data.id;
+                    cart.getProducts().length = 0;
+                })
+                .catch(function (error) {  // failed
+                    $scope.data.orderError = error;
+                })
+                .finally(function () {
+                    $location.path("/complete");
+                })
+        }
+
+    });
